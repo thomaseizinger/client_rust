057b4834ea4d31afa679681a7ce8fa2395f37df7
Thomas Eizinger <thomas@eizinger.io>
2022-11-21 17:23:20 +1100

    Fix benchmarks

--- benches/encoding/proto.rs
+++ benches/encoding/proto.rs
@@ -2,24 +2,24 @@
 // https://github.com/tikv/rust-prometheus/blob/ab1ca7285d3463504381a5025ae1951e020d6796/benches/text_encoder.rs:write
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use prometheus_client::encoding::protobuf::{encode, EncodeMetric};
-use prometheus_client::encoding::Encode;
+use prometheus_client::encoding::protobuf::encode;
 use prometheus_client::metrics::counter::Counter;
 use prometheus_client::metrics::family::Family;
 use prometheus_client::metrics::histogram::{exponential_buckets, Histogram};
 use prometheus_client::registry::Registry;
+use prometheus_client_derive_encode::{EncodeLabelSet, EncodeLabelValue};
 use std::fmt::{Display, Formatter};
 
 pub fn proto(c: &mut Criterion) {
     c.bench_function("encode", |b| {
-        #[derive(Clone, Hash, PartialEq, Eq, Encode)]
-        struct Labels {
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelSet)]
+        struct CounterLabels {
             path: String,
             method: Method,
             some_number: u64,
         }
 
-        #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelValue)]
         enum Method {
             Get,
             #[allow(dead_code)]
@@ -35,35 +35,41 @@ pub fn proto(c: &mut Criterion) {
             }
         }
 
-        #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelSet)]
+        struct HistogramLabels {
+            region: Region,
+        }
+
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelValue)]
         enum Region {
             Africa,
             #[allow(dead_code)]
             Asia,
         }
 
-        let mut registry = Registry::<Box<dyn EncodeMetric>>::default();
+        let mut registry = Registry::default();
 
         for i in 0..100 {
-            let counter_family = Family::<Labels, Counter>::default();
-            let histogram_family = Family::<Region, Histogram>::new_with_constructor(|| {
-                Histogram::new(exponential_buckets(1.0, 2.0, 10))
-            });
+            let counter_family = Family::<CounterLabels, Counter>::default();
+            let histogram_family =
+                Family::<HistogramLabels, Histogram>::new_with_constructor(|| {
+                    Histogram::new(exponential_buckets(1.0, 2.0, 10))
+                });
 
             registry.register(
                 format!("my_counter{}", i),
                 "My counter",
-                Box::new(counter_family.clone()),
+                counter_family.clone(),
             );
             registry.register(
                 format!("my_histogram{}", i),
                 "My histogram",
-                Box::new(histogram_family.clone()),
+                histogram_family.clone(),
             );
 
             for j in 0_u32..100 {
                 counter_family
-                    .get_or_create(&Labels {
+                    .get_or_create(&CounterLabels {
                         path: format!("/path/{}", i),
                         method: Method::Get,
                         some_number: j.into(),
@@ -71,14 +77,16 @@ pub fn proto(c: &mut Criterion) {
                     .inc();
 
                 histogram_family
-                    .get_or_create(&Region::Africa)
+                    .get_or_create(&HistogramLabels {
+                        region: Region::Africa,
+                    })
                     .observe(j.into());
             }
         }
 
         b.iter(|| {
             let metric_set = encode(&registry);
-            black_box(metric_set);
+            black_box(metric_set).unwrap();
         })
     });
 }
--- benches/encoding/text.rs
+++ benches/encoding/text.rs
@@ -1,31 +1,32 @@
 // Benchmark inspired by https://github.com/tikv/rust-prometheus/blob/ab1ca7285d3463504381a5025ae1951e020d6796/benches/text_encoder.rs
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use prometheus_client::encoding::text::{encode, EncodeMetric};
-use prometheus_client::encoding::Encode;
+use prometheus_client::encoding::text::encode;
+use prometheus_client::encoding::LabelValueEncoder;
 use prometheus_client::metrics::counter::Counter;
 use prometheus_client::metrics::family::Family;
 use prometheus_client::metrics::histogram::{exponential_buckets, Histogram};
 use prometheus_client::registry::Registry;
-use std::io::Write;
+use prometheus_client_derive_encode::{EncodeLabelSet, EncodeLabelValue};
+use std::fmt::{Error, Write};
 
 pub fn text(c: &mut Criterion) {
     c.bench_function("encode", |b| {
-        #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelSet)]
         struct Labels {
             method: Method,
             status: Status,
             some_number: u64,
         }
 
-        #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+        #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelValue)]
         enum Method {
             Get,
             #[allow(dead_code)]
             Put,
         }
 
-        #[derive(Clone, Hash, PartialEq, Eq)]
+        #[derive(Debug, Clone, Hash, PartialEq, Eq)]
         enum Status {
             Two,
             #[allow(dead_code)]
@@ -34,34 +35,19 @@ pub fn text(c: &mut Criterion) {
             Five,
         }
 
-        impl prometheus_client::encoding::text::Encode for Status {
-            fn encode(&self, writer: &mut dyn Write) -> Result<(), std::fmt::Error> {
-                let status = match self {
-                    Status::Two => b"200",
-                    Status::Four => b"400",
-                    Status::Five => b"500",
-                };
-                writer.write_all(status)?;
+        impl prometheus_client::encoding::EncodeLabelValue for Status {
+            fn encode(&self, encoder: &mut LabelValueEncoder) -> Result<(), Error> {
+                encoder.write_str(match self {
+                    Status::Two => "200",
+                    Status::Four => "400",
+                    Status::Five => "500",
+                })?;
+
                 Ok(())
             }
         }
 
-        #[cfg(feature = "protobuf")]
-        impl prometheus_client::encoding::protobuf::EncodeLabels for Status {
-            fn encode(&self, labels: &mut Vec<prometheus_client::encoding::protobuf::Label>) {
-                let value = match self {
-                    Status::Two => "200".to_string(),
-                    Status::Four => "400".to_string(),
-                    Status::Five => "500".to_string(),
-                };
-                labels.push(prometheus_client::encoding::protobuf::Label {
-                    name: "status".to_string(),
-                    value,
-                });
-            }
-        }
-
-        let mut registry = Registry::<Box<dyn EncodeMetric>>::default();
+        let mut registry = Registry::default();
 
         for i in 0..100 {
             let counter_family = Family::<Labels, Counter>::default();
@@ -72,12 +58,12 @@ pub fn text(c: &mut Criterion) {
             registry.register(
                 format!("my_counter_{}", i),
                 "My counter",
-                Box::new(counter_family.clone()),
+                counter_family.clone(),
             );
             registry.register(
                 format!("my_histogram_{}", i),
                 "My histogram",
-                Box::new(histogram_family.clone()),
+                histogram_family.clone(),
             );
 
             for j in 0u32..100 {
@@ -98,7 +84,7 @@ pub fn text(c: &mut Criterion) {
             }
         }
 
-        let mut buffer = vec![];
+        let mut buffer = String::new();
 
         b.iter(|| {
             encode(&mut buffer, &registry).unwrap();
